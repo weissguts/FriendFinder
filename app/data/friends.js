1716983f05ff8a25fd55cd78ab55dcc239ffdab6
Daniel Hernandez <weissguts@gmail.com>
2018-04-01 19:36:01 -0600

    finally found function to properly get sum of values of array in an array (values from survey)

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,51 @@
 var preFriendsArray = [
     {
         "name":"Ahmed",
-        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo":"https://images.pexels.com/photos/428339/pexels-photo-428339.jpeg?auto=compress&cs=tinysrgb&h=350",
         "scores":[
             5,
-            1,
             4,
             4,
             5,
-            1,
+            3,
             2,
-            5,
-            4,
-            1
+            1,
+            1,
+            1,
+            2
             ]
+    },
+    {
+        "name":"Susy",
+        "photo":"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=350",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        "name":"Rachel",
+        "photo":"https://images.pexels.com/photos/712521/pexels-photo-712521.jpeg?auto=compress&cs=tinysrgb&h=350",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
     }
 ];
 // Note how we export the array. This makes it accessible to other files using require.
